491654f3d199149b895266f019600eab4ea516db
AbubakkarCraft <mdabubakkarrayeen@gmail.com>
2026-02-23 21:05:30 +0545

    added controller for books

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,8 +1,31 @@
-const Book = require('../models/Book');
+const { Book, Rating, sequelize } = require('../models');
 
 const getBooks = async (req, res) => {
     try {
-        const books = await Book.findAll();
+        const books = await Book.findAll({
+            attributes: {
+                include: [
+                    [
+                        sequelize.literal(`(
+                            SELECT AVG(score)
+                            FROM "Ratings" AS "Rating"
+                            WHERE
+                                "Rating"."bookId" = "Book"."id"
+                        )`),
+                        'averageRating'
+                    ],
+                    [
+                        sequelize.literal(`(
+                            SELECT COUNT(*)
+                            FROM "Ratings" AS "Rating"
+                            WHERE
+                                "Rating"."bookId" = "Book"."id"
+                        )`),
+                        'ratingCount'
+                    ]
+                ]
+            }
+        });
         res.status(200).json(books);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching books', error: error.message });
@@ -11,7 +34,30 @@ const getBooks = async (req, res) => {
 
 const getBookById = async (req, res) => {
     try {
-        const book = await Book.findByPk(req.params.id);
+        const book = await Book.findByPk(req.params.id, {
+            attributes: {
+                include: [
+                    [
+                        sequelize.literal(`(
+                            SELECT AVG(score)
+                            FROM "Ratings" AS "Rating"
+                            WHERE
+                                "Rating"."bookId" = "Book"."id"
+                        )`),
+                        'averageRating'
+                    ],
+                    [
+                        sequelize.literal(`(
+                            SELECT COUNT(*)
+                            FROM "Ratings" AS "Rating"
+                            WHERE
+                                "Rating"."bookId" = "Book"."id"
+                        )`),
+                        'ratingCount'
+                    ]
+                ]
+            }
+        });
         if (!book) {
             return res.status(404).json({ message: 'Book not found' });
         }
