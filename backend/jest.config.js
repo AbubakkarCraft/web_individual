adb206b813583774f0c1fc4f9a01762a1d7a9e44
AbubakkarCraft <mdabubakkarrayeen@gmail.com>
2026-02-26 20:58:12 +0545

    testing file

---
+++ backend/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testEnvironment: 'node',
+    setupFilesAfterEnv: ['./tests/setup.js'],
+    verbose: true,
+    forceExit: true,
+    clearMocks: true,
+    resetModules: true,
+    restoreMocks: true,
+};
